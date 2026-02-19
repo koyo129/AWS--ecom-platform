fcc304d3c4dacefbbf1d4a3920a4898436543497
Kohyo Miyajima <kohyo0129@gmail.com>
2026-02-19 15:13:06 +0900

    Create security.tf

---
+++ terraform/security.tf
@@ -0,0 +1,39 @@
+# ALB Security Group (Public Entrance)
+resource "aws_security_group" "alb_sg" {
+  name        = "alb-security-group"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# EC2 Security Group (Internal Trust Only)
+resource "aws_security_group" "ec2_sg" {
+  name        = "ec2-security-group"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
