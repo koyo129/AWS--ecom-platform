c5e8ffce0ff0f84a37aa518dd7ebadc10949e1a3
Kohyo Miyajima <kohyo0129@gmail.com>
2026-04-02 14:16:13 +0900

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "db_username" {
+  description = "Database username"
+  type        = string
+  default     = "adminuser"
+}
+
+variable "db_password" {
+  description = "Database password"
+  type        = string
+  sensitive   = true
+}
