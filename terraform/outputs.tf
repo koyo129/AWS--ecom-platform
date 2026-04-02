e6323f992f61f1d4b3f49128e1b0239dd90b28ab
Kohyo Miyajima <kohyo0129@gmail.com>
2026-04-02 20:45:31 +0900

    Add RDS endpoint output to outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,3 +2,8 @@ output "website_url" {
   description = "The URL of the website"
   value       = "http://${aws_lb.app.dns_name}"
 }
+
+output "rds_endpoint" {
+  description = "The RDS endpoint"
+  value       = aws_db_instance.postgres.address
+}
