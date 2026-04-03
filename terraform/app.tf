fa3e769279cd63055bd8c6a8444d716ec8f82cdc
Kohyo Miyajima <kohyo0129@gmail.com>
2026-04-03 19:20:36 +0900

    Update app.tf

--- terraform/app.tf
+++ terraform/app.tf
@@ -35,6 +35,10 @@ resource "aws_launch_template" "web" {
   update_default_version = true
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
 
+iam_instance_profile {
+  name = aws_iam_instance_profile.ec2_profile.name
+}
+
   metadata_options {
     http_endpoint = "enabled"
     http_tokens   = "required"
@@ -78,4 +82,29 @@ resource "aws_autoscaling_group" "asg" {
     value               = "CloudCommerce-Web"
     propagate_at_launch = true
   }
+}
+
+# IAM Role for EC2
+resource "aws_iam_role" "ec2_role" {
+  name = "ec2-ssm-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action    = "sts:AssumeRole"
+      Effect    = "Allow"
+      Principal = { Service = "ec2.amazonaws.com" }
+    }]
+  })
+}
+
+# Attach SSM policy to role
+resource "aws_iam_role_policy_attachment" "ssm_policy" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+# Instance profile
+resource "aws_iam_instance_profile" "ec2_profile" {
+  name = "ec2-ssm-profile"
+  role = aws_iam_role.ec2_role.name
 }
