7ceddd88719dcfba888cc44cf4beb87f5eee7f70
Kohyo Miyajima <kohyo0129@gmail.com>
2026-04-06 05:42:09 +0900

    Update app.tf

--- terraform/app.tf
+++ terraform/app.tf
@@ -8,7 +8,7 @@ resource "aws_lb" "app" {
 
 resource "aws_lb_target_group" "tg" {
   name     = "app-target-group"
-  port     = 80
+  port     = 5000
   protocol = "HTTP"
   vpc_id   = aws_vpc.main.id
   health_check {
